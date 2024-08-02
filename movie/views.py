887b5d969bd2051e6e554a1eef00140d67fc6e88
Salome-Serna-R <124018168+Salome-Serna-R@users.noreply.github.com>
2024-08-01 21:05:00 -0500

    codigo busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -10,7 +10,10 @@ def home(request):
     #return render(request, 'home.html')
     #return render(request, 'home.html', {'name': 'Salomé Serna'})
     searchTerm = request.GET.get('searchMovie')
-    movies = Movie.objects.all()
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
     return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
